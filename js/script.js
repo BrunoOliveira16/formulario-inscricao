0d028c97af0ffdbc0346a7fb4c5862fbcc7bc2a3
Bruno <bruno1_oliveira@hotmail.com>
2022-07-26 20:47:10 -0300

    Etapa 03 concluida - Atualizacao responsividade da pagina com tamanho de 990px - 360px

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@ document.getElementById("form-00").addEventListener("submit", function(evento){
     evento.stopPropagation();
 
     console.log(evento)
-    document.getElementById("Envio").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "Data sent Successfully";
 
 });
+
+/* Validação campo em branco */
+
+
+
+
+let CampoObrigatorio = document.querySelectorAll("input");
+
+for(let emFoco of CampoObrigatorio) {
+    validaCampo(emFoco);
+}
